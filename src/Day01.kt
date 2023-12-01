0bb1b0a3ca81465d2c31580c3fdd9a6fe3d0cc5a
Julien Bardel <julien.bardel@harvest.fr>
2023-12-01 18:16:10 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,63 @@
+val textToNumberMapping = mapOf(
+    "one" to "o1e",
+    "two" to "t2o",
+    "three" to "t3e",
+    "four" to "f4r",
+    "five" to "f5e",
+    "six" to "s6x",
+    "seven" to "s7n",
+    "eight" to "e8t",
+    "nine" to "n9e"
+)
+
 fun main() {
+
+    fun getNumbersFromLine(line: String): String {
+        val numbers = line.filter { it in '0'..'9' }.fold("") { c1, c2 -> "$c1$c2" }
+        return when (numbers.length) {
+            0 -> "0"
+            1 -> "${numbers.first()}${numbers.first()}"
+            else -> "${numbers.first()}${numbers.last()}"
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { getNumbersFromLine(it) }.sumOf { it.toInt() }
+    }
+
+    fun nextNumberToReplace(line: String): String {
+        return textToNumberMapping
+            .map { Pair(line.indexOf(it.key), it) }
+            .filter { it.first > -1 }
+            .minByOrNull { it.first }?.second?.key ?: ""
+    }
+
+    fun replaceWords(line: String): String {
+        var l = line
+        var word = nextNumberToReplace(l)
+        while (word.isNotEmpty()) {
+            l = l.replace(word, textToNumberMapping[word]!!)
+            word = nextNumberToReplace(l)
+        }
+        return l
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+//            .onEach { print("$it -> ") }
+            .map { replaceWords(it) }
+//            .onEach { print("$it -> ") }
+            .map { getNumbersFromLine(it) }
+//            .onEach { println(it) }
+            .map { it.toInt() }
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
     part2(input).println()
 }
